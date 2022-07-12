aadd38db31ac1ed785cdb866cdb30ce5c244ee06
Tiago Pereira <tiago.pereira@triersistemas.com.br>
2022-07-12 13:40:36 -0300

    cliente

--- src/main/java/br/com/triersistemas/andromeda/model/ClienteModel.java
+++ src/main/java/br/com/triersistemas/andromeda/model/ClienteModel.java
@@ -1,12 +1,10 @@
 package br.com.triersistemas.andromeda.model;
 
 import br.com.triersistemas.andromeda.domain.Cliente;
-import br.com.triersistemas.andromeda.helper.CpfUtils;
 import br.com.triersistemas.andromeda.helper.StringUtils;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
-import org.hibernate.validator.constraints.br.CPF;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
@@ -38,31 +36,20 @@ public class ClienteModel {
     @NotNull
     private String cpf;
 
+    private String documento;
+    private Long idade;
+    private Boolean documentoValido;
+
     public ClienteModel(Cliente cliente) {
         this.id = cliente.getId();
         this.nome = cliente.getNome();
         this.niver = cliente.getNiver();
         this.email = cliente.getEmail();
         this.cpf = cliente.getDocumento();
-    }
 
-    public boolean getDocumentoValido() {
-        final List<Integer> digitos = StringUtils.extractNumbersToList(this.cpf);
-        if (digitos.size() == 11 && digitos.stream().distinct().count() > 1) {
-            return CpfUtils.geraCpf(digitos.subList(0, 9)).equals(this.cpf);
-        }
-        return false;
-    }
-
-    public String getDocumento() {
-        if (this.cpf.length() == 11) {
-            return cpf.replaceAll("(\\d{3})(\\d{3})(\\d{3})(\\d{2})", "$1.$2.$3-$4");
-        }
-        return cpf;
-    }
-
-    public Long getIdade() {
-        return Objects.nonNull(niver) ? ChronoUnit.YEARS.between(niver, LocalDate.now()) : 0L;
+        this.documento = cliente.getDocumento();
+        this.idade = cliente.getIdade();
+        this.documentoValido = cliente.getDocumentoValido();
     }
 
 }
